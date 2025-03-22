e5364d000c1d82a3b04a9fd6cffe6548b98ce653
Preda Vlad <133563696+Mrpokemon04@users.noreply.github.com>
2025-03-22 12:34:38 +0200

    Update hi.rs

--- src/bin/hi.rs
+++ src/bin/hi.rs
@@ -1,25 +1,39 @@
 // +---------------------------------------------------------------------------+
-// |                            Embassy March 2025                             |
-// |                            Hi, I am new here!                             |
+// |                             PM/MA lab skel                                |
 // +---------------------------------------------------------------------------+
-
+ 
+//! By default, this app prints a "Hello world" message with `defmt`.
+ 
 #![no_std]
 #![no_main]
-
+ 
 use embassy_executor::Spawner;
-// Use the `panic_probe` crate to provided the panic handler and the 
-// `defmt_rtt` to import the runtime for defmt, to be able to use
-// the print macros.
+use embassy_rp::{gpio::{Input, Level, Output, Pull}, pwm::Pwm};
+use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 // Use the logging macros provided by defmt.
-#[allow(unused)]
 use defmt::*;
-
+ 
+// PWM config
+use embassy_rp::pwm::Config as ConfigPwm; 
+ 
+// Import interrupts definition module
+mod irqs;
+ 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     // Get a handle to the RP's peripherals.
-    let _p = embassy_rp::init(Default::default());
+    let peripherals = embassy_rp::init(Default::default());
 
+    let mut switch4 = Input::new(peripherals.PIN_6, Pull::None);
+    let mut switch5 = Input::new(peripherals.PIN_7, Pull::None);
+    let mut switch6 = Input::new(peripherals.PIN_8, Pull::None);
+    let mut switch7 = Input::new(peripherals.PIN_9, Pull::None);    
+ 
     loop {
+        if switch4.is_low() || switch5.is_low() || switch6.is_low() || switch7.is_low() {
+            println!("Mr. Frog Supporters");
+            Timer::after_millis(200).await;
+        }
     }
 }
